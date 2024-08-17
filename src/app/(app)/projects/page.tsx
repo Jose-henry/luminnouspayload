1f1241fca70d4264440e908c8bacf3ece66fbc68
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-08-17 10:38:56 +0100

    dynamic renderng

--- src/app/(app)/projects/page.tsx
+++ src/app/(app)/projects/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 
 //export const dynamic = 'force-dynamic'
 
-export const revalidate = 3600 // 1 hour
+export const revalidate = 1800 // 30 minutes
 
 
 export const metadata: Metadata = {
