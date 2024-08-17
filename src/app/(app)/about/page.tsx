f7251ef3009ab4ba5fb7241aeb374e8d7e7810db
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-08-17 20:58:55 +0100

    optimizing

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 
 //export const dynamic = 'force-dynamic'
 
-export const revalidate = 0 // 1 hr
+export const revalidate = 120 // 1 hr
 
 
 export const metadata: Metadata = {
