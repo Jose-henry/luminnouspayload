34095c4b009b3a0044e9d939c0a9822d9fc05677
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-08-17 12:55:03 +0100

    add cron

--- src/app/(app)/api/cron/route.ts
+++ src/app/(app)/api/cron/route.ts
@@ -4,6 +4,6 @@ export const revalidate = 0
 
 export async function GET() {
 
-    await fetch(`https://${process.env.VERCEL_URL}/admin`)
+    await fetch(`https://${process.env.VERCEL_URL}/admin`);
   return NextResponse.json({ ok: true });
 }
