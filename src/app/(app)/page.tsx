e50c4611fe990538a461c0f3479e6f1f6cf4e522
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-08-17 20:56:33 +0100

    optimizing

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,7 +2,7 @@ import ServerHome from "@/components/home_components/ServerHome";
 
 
 //export const dynamic = 'force-dynamic'
-export const revalidate = 3600 // 1 hr
+export const revalidate = 0 // 1 hr
 
 export default function Home() {
   return (
