b35387cd0dc3a5f11783c2d7f3d991d0675e9dcc
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-08-19 18:12:09 +0100

    dynamic

--- src/components/project_components/ClientProject.tsx
+++ src/components/project_components/ClientProject.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react';
 
 //import Gallery from "@/components/project_components/gallery_component/gallery";
-import Intro from "@/components/project_components/intro_component/intro";
+//import Intro from "@/components/project_components/intro_component/intro";
 //import Outline from "@/components/project_components/project_outline/outline";
 import dynamic from 'next/dynamic';
 
