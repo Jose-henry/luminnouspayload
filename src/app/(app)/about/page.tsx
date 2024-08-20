98251f013244a48e7dc917ebe647c17841ec3fac
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-08-20 12:36:10 +0100

    dynamic

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -1,9 +1,12 @@
-import ServerAbout from "@/components/about_components/ServerAbout";
+//import ServerAbout from "@/components/about_components/ServerAbout";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 //export const dynamic = 'force-dynamic'
 
-export const revalidate = 60 // 1 week
+//export const revalidate = 60 // 1 week
+
+const ServerAbout = dynamic(() => import('@/components/about_components/ServerAbout'), { ssr: true })
 
 
 export const metadata: Metadata = {
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,10 @@
-import ServerHome from "@/components/home_components/ServerHome";
+//import ServerHome from "@/components/home_components/ServerHome";
+import dynamic from "next/dynamic";
 
 
+const ServerHome = dynamic(() => import('@/components/home_components/ServerHome'), { ssr: false })
 //export const dynamic = 'force-dynamic'
-export const revalidate = 120 //604800 // 1 week
+//export const revalidate = 120 //604800 // 1 week
 
 export default function Home() {
   return (
